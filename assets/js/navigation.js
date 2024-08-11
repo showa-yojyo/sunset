54fe2c74ad8ba4c69f79f739ff4e1d9d4be71378
showa_yojyo <2386769+showa-yojyo@users.noreply.github.com>
2024-08-11 21:58:21 +0900

    assets/js/navigation.js: double-tap navigation only on mobile browsers
    Signed-off-by: showa_yojyo <2386769+showa-yojyo@users.noreply.github.com>

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -32,31 +32,37 @@ document.addEventListener('keydown', event => {
     event.preventDefault();
 });
 
-const SWIPE_THRESHOLD = 200;
-const pointerTarget = document.querySelector('main');
+// Reference: Vanilla JavaScript Double tap event detection on mobile using
+// setTimeout
+// <https://gist.github.com/ethanny2/44d5ad69970596e96e0b48139b89154b>
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    const pointerTarget = document.body;
+    const DOUBLE_TAP_DURATION = 500;
+    let timeout = 0;
+    let lastTap = new Date().getTime();
 
-pointerTarget.addEventListener("pointerdown", (eDown) => {
-    function onPointerUp(eUp){
-        //pointerTarget.releasePointerCapture(eUp.Id);
-        pointerTarget.removeEventListener("pointerup", onPointerUp)
-        //console.debug(`Pointer up ${eUp.clientX} ${eUp.clientY}`);
+    pointerTarget.addEventListener('pointerup', (event) => {
+        const currentTime = new Date().getTime();
+        const tapLength = currentTime - lastTap;
+        lastTap = currentTime;
 
-        const diff = eUp.clientX - eDown.clientX;
-        if(Math.abs(diff) < SWIPE_THRESHOLD){
-            return;
+        if (0 < tapLength && tapLength < DOUBLE_TAP_DURATION) {
+            event.preventDefault();
+            event.target.dispatchEvent(new CustomEvent("doubletap", {
+                bubbles: true,
+                detail: event
+            }));
+        } else {
+            timeout = setTimeout(() => clearTimeout(timeout), DOUBLE_TAP_DURATION);
         }
+    }, {passive: false});
 
-        const rel = diff > 0 ? 'prev' : 'next';
-        //console.debug(rel)
+    pointerTarget.addEventListener('doubletap', (event) => {
+        const rel = event.detail.screenX < screen.width / 2 ? 'prev' : 'next';
         const link = document.querySelector(`link[rel="${rel}"]`);
         if (!link){
-             return;
+            return;
         }
         document.location = link.href;
-        eUp.preventDefault();
-    }
-
-    //console.debug(`Pointer down ${eDown.clientX} ${eDown.clientY}`);
-    //pointerTarget.setPointerCapture(eDown.Id);
-    pointerTarget.addEventListener("pointerup", onPointerUp);
-});
+    });
+}
